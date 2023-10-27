b11515a02e09bad79183722a8f24b4293c29b933
ViniTex231 <viniteixeira2828@gmail.com>
2023-10-27 10:33:26 -0300

    f

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [userId, setUserId] = useState(0)
+  const [usuario, setUsuario] = useState('')
+  const [cidade, setCidade] = useState('')
+  const [errou, setErro] = useState('')
+  const [userAdd, setUserAdd] = useState('')
+  const [cityAdd, setCityAdd] = useState('')
+
+  const buscar = ()=>{
+    axios.get('http://127.0.0.1:8000/api/v1/usuario/'+userId+'/')
+    .then((res)=>{
+      setUsuario(res.data.nome)
+      setCidade(res.data.cidade)
+    }).catch((erro)=>{
+      console.log(erro.response.statusText)
+    })
+  }
+
+  const adicionar = ()=>{
+    axios.post('http://127.0.0.1:8000/api/v1/usuarios/',
+    {
+      nome:userAdd,
+      cidade:cityAdd
+    }
+    ).then((res)=>{
+      console.log(res.response.nome)
+      setUserAdd('')
+    }).catch((erro)=>{
+      console.log(erro)
+    })
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto2}>ID:</Text>
+      <TextInput
+        style={styles.ID}
+        onChangeText={(e) => setUserId(e)}
+      />
+      <View style={styles.btn}>
+        <Button
+          title='GET'
+          color='red'
+          onPress={buscar}
+        />
+      </View>
+      <Text style={styles.texto2}>Nome:</Text>
+      <Text style={styles.texto}>{usuario}</Text>
+      <Text style={styles.texto2}>Cidade:</Text>
+      <Text style={styles.texto}>{cidade}</Text>
+
+      <Text style={styles.texto2}>Usuário:</Text>
+      <TextInput
+        style={styles.addNew}
+        onChangeText={(e) => setUserAdd(e)}
+        value={userAdd}
+      />
+      <Text style={styles.texto2}>Cidade:</Text>
+      <TextInput
+        style={styles.addNew}
+        onChangeText={(e) => setCityAdd(e)}
+      />
+
+      <View style={styles.btn}>
+        <Button
+          title='POST'
+          color='blue'
+          onPress={adicionar}
+        />
+      </View>
     </View>
   );
 }
@@ -14,7 +81,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft: 30
   },
+  ID: {
+    width: '20%',
+    height: 40,
+    borderRadius: 10,
+    borderWidth: 1,
+    paddingLeft:10,
+    fontWeight: 'bold'
+  },
+  addNew: {
+    width: '80%',
+    height: 40,
+    borderRadius: 10,
+    borderWidth: 1,
+    paddingLeft:10,
+    fontWeight: 'bold'
+  },
+  texto: {
+    width: '80%',
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: '#bbb',
+    borderWidth: 1,
+    padding:10,
+    fontWeight: 'bold'
+  },
+  texto2: {
+    marginTop: 10,
+    marginLeft: 10
+  },
+  btn: {
+    width: 100,
+    height: 40,
+    marginTop: 10,
+  }
 });
